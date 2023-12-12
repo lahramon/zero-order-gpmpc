c6c48d7c7e9b16f1826453a60c917e453231ca07
Amon Lahr <amlahr@ethz.ch>
2023-12-12 15:05:45 +0100

    Add documentation to pendulum example

--- examples/inverted_pendulum/inverted_pendulum_zoro_acados.py
+++ examples/inverted_pendulum/inverted_pendulum_zoro_acados.py
@@ -1,6 +1,7 @@
 # ---
 # jupyter:
 #   jupytext:
+#     formats: ipynb,py
 #     text_representation:
 #       extension: .py
 #       format_name: light
@@ -36,7 +37,21 @@ from gpytorch_utils.gp_model import MultitaskGPModel, BatchIndependentMultitaskG
 
 # -
 
+# ## Define model parameters
 #
+# We model the inverted pendulum
+#
+# $$
+# \dot{x} = f(x,u) = \begin{bmatrix} \dot{\theta} \\ \ddot{\theta} \end{bmatrix} = \begin{bmatrix} \dot{\theta} \\ -\sin(\theta) + u \end{bmatrix},
+# $$
+#
+# which is to be controlled from the hanging-down resting position, $(\theta_0, \dot{\theta}_0) = (\pi, 0)$, to the upright position ($(\theta_r, \dot{\theta}_r) = (0,0)$), subject to the constraints that overshoot should be avoided, i.e.,
+#
+# $$
+# \theta_{lb} \leq \theta \leq \theta_{ub}.
+# $$
+#
+# The model setup and controller definition can be found in the functions `export_simplependulum_ode_model()`, `export_ocp_nominal()` in the `inverted_pendulum_model_acados.py` file.
 
 # +
 # discretization
@@ -44,36 +59,23 @@ N = 30
 T = 5
 dT = T / N
 
-# satisfaction probability for chance constraints
-prob_x = 0.9
-prob_tighten = norm.ppf(prob_x)
-
 # constraints
 x0 = np.array([np.pi, 0])
 nx = 2
 nu = 1
 
-# noise
-# uncertainty dynamics
-sigma_theta = (0.0001/360.) * 2 * np.pi
-sigma_omega = (0.0001/360.) * 2 * np.pi
-w_theta = 0.03
-w_omega = 0.03
-Sigma_x0 = np.array([
-    [sigma_theta**2,0],
-    [0,sigma_omega**2]
-])
-Sigma_W = np.array([
-    [w_theta**2, 0],
-    [0, w_omega**2]
-])
+# -
 
+# ## Set up nominal solver
 
 # +
 ocp_init = export_ocp_nominal(N,T,only_lower_bounds=True)
 ocp_init.solver_options.nlp_solver_type = "SQP"
 
 acados_ocp_init_solver = AcadosOcpSolver(ocp_init, json_file="acados_ocp_init_simplependulum_ode.json")
+# -
+
+# ## Open-loop planning with nominal solver
 
 # +
 # get initial values
@@ -95,6 +97,9 @@ for i in range(N):
     U_init[i,:] = acados_ocp_init_solver.get(i, "u")
 
 X_init[N,:] = acados_ocp_init_solver.get(N, "x")
+# -
+
+# ## Simulation results (nominal)
 
 # +
 lb_theta = -ocp_init.constraints.lh[0]
@@ -107,12 +112,66 @@ plot_data_nom = EllipsoidTubeData2D(
 add_plot_trajectory(ax, plot_data_nom, prob_tighten=prob_tighten)
 # -
 
+# ## Robustify simulation
+#
+# Now we consider the inverted pendulum subject to process noise, i.e., after discretization, 
+#
+# $$
+# x_{k+1} = F(x_k, u_k) + w_k,
+# $$
+#
+# where $w_k \sim \mathcal{N}(0, \Sigma_w)$ denotes process noise and we jhave a potentially nois initial condition $x_0 \sim \mathcal{N}(0, \Sigma_{x0})$ (defined below). Constraints should now be satisfied with satisfaction probability $p_x$, i.e.,
+#
+# $$
+# \mathrm{Pr}(h(x,u) \leq 0) \geq p_x.
+# $$
+#
+# Note that currently zoRO only accepts upper bounds, i.e., we need to reformulate the nominal constraints $\theta \leq \theta_{ub}$, $-\theta \leq -\theta_{lb}$. 
+# Afterwards, we consider the tightened constraints
+#
+# $$
+# h(x,u) + \alpha \sqrt{C(x,u) P C(x,u)} \leq 0,
+# $$
+#
+# see problem definition https://www.sciencedirect.com/science/article/pii/S0947358023000912, eq. (9).
+
+# +
+# satisfaction probability for chance constraints
+prob_x = 0.9
+prob_tighten = norm.ppf(prob_x)
+
+# noise
+# uncertainty dynamics
+sigma_theta = (0.0001/360.) * 2 * np.pi
+sigma_omega = (0.0001/360.) * 2 * np.pi
+w_theta = 0.03
+w_omega = 0.03
+Sigma_x0 = np.array([
+    [sigma_theta**2,0],
+    [0,sigma_omega**2]
+])
+Sigma_W = np.array([
+    [w_theta**2, 0],
+    [0, w_omega**2]
+])
+# -
+
+# ### Nominal OCP with lower bounds
+
+# +
 # without gp model
 ocp_zoro_nogp = export_ocp_nominal(N, T, only_lower_bounds=True)
+
+# OCP is discretized inside zoRO solver, so we need to choose DISCRETE integration here
 ocp_zoro_nogp.solver_options.integrator_type = "DISCRETE"
 ocp_zoro_nogp.solver_options.qp_solver = "PARTIAL_CONDENSING_HPIPM"
 ocp_zoro_nogp.solver_options.hessian_approx = "GAUSS_NEWTON"
 ocp_zoro_nogp.solver_options.nlp_solver_type = "SQP_RTI"
+# -
+
+# ### Nominal model with lower bounds
+#
+# The tightenings $\sqrt{\ldots}$ can thereby be automatically defined using the `tighten_model_constraints()` function. 
 
 # +
 # zoro_solver_nogp = ZoroAcados(ocp_zoro_nogp, sim, prob_x, Sigma_x0, Sigma_W+Sigma_GP_prior)
@@ -130,7 +189,11 @@ ocp_zoro_nogp.model = ocp_model_tightened
 ocp_zoro_nogp.dims.nh = ocp_model_tightened.con_h_expr.shape[0]
 ocp_zoro_nogp.dims.np = ocp_model_tightened.p.shape[0]
 ocp_zoro_nogp.parameter_values = np.zeros((ocp_zoro_nogp.dims.np,))
+# -
 
+# ## Simulator object
+#
+# To automatically discretize the model (and obtain sensitivities of the discrete-time model) within the zero-order implementation, we create the `AcadosSimSolver` object to pass to the solver.
 
 # +
 # integrator for nominal model
@@ -145,6 +208,18 @@ sim.solver_options.T = dT
 
 # acados_ocp_solver = AcadosOcpSolver(ocp_init, json_file = 'acados_ocp_' + model.name + '.json')
 acados_integrator = AcadosSimSolver(sim, json_file = 'acados_sim_' + sim.model.name + '.json')
+# -
+
+# ## Create zero-order solver (without GP)
+#
+# Everything comes together in the `ZoroAcados` constructor: 
+#
+# - nominal OCP `ocp_zoro_nogp`, 
+# - simulator `AcadosSim` object `sim`,
+# - satisfaction probability `prob_x`,
+# - initial uncertainty `Sigma_x0`,
+# - process noise `Sigma_W`,
+# - Jacobian of tightened constraints w.r.t. covariances $P$, `h_tighten_jac_sig_fun`
 
 # +
 # solve with zoRO (no GP model, only process noise)
@@ -160,6 +235,11 @@ zoro_solver_nogp.ocp_solver.set(N, "x",X_init[N,:])
 
 zoro_solver_nogp.solve()
 X_nogp,U_nogp,P_nogp = zoro_solver_nogp.get_solution()
+# -
+
+# ### Plot open-loop prediction
+#
+# Ellipsoids denote the Gaussian confidence intervals, note that one (terminal) ellipsoid "sticks out" since we have omitted the terminal constraint here.
 
 # +
 fig, ax = base_plot(lb_theta=lb_theta)
@@ -171,6 +251,11 @@ plot_data_nogp = EllipsoidTubeData2D(
 )
 add_plot_trajectory(ax, plot_data_nom, color_fun=plt.cm.Blues, prob_tighten=prob_tighten)
 add_plot_trajectory(ax, plot_data_nogp, color_fun=plt.cm.Oranges, prob_tighten=prob_tighten)
+# -
+
+# # GP training
+#
+# We use a model with different parameters to emulate the real-world model and obtain some training data. Also create simulator object for real-world model to evaluate our results later (not used in solver).
 
 # +
 # generate training data for GP with augmented model
@@ -194,6 +279,11 @@ sim_actual.solver_options.T = dT
 
 # acados_ocp_solver = AcadosOcpSolver(ocp, json_file = 'acados_ocp_' + model.name + '.json')
 acados_integrator_actual = AcadosSimSolver(sim_actual, json_file = 'acados_sim_' + model_actual.name + '.json')
+# -
+
+# ## Generate training data
+#
+# We generate training data (one-step ahead residuals `y_train` for starting point `x_train`) here by running robustified (cautious) solver without GP.
 
 # +
 random_seed = 123
@@ -216,6 +306,11 @@ y_train = generate_train_outputs_at_inputs(
     acados_integrator_actual, 
     Sigma_W
 )
+# -
+
+# ## Hyper-parameter training for GP model
+#
+# Optimize hyper-parameters of GP model (kernel function parameters, ...)
 
 # +
 x_train_tensor = torch.Tensor(x_train)
@@ -236,6 +331,11 @@ gp_model, likelihood = train_gp_model(gp_model, torch_seed=rng_seed, training_it
 # EVAL MODE
 gp_model.eval()
 likelihood.eval()
+# -
+
+# ## Plot GP predictions
+#
+# We plot GP predictions along the predicted trajectory of the robustified solver by projecting the multivariate plot down to a line.
 
 # +
 num_samples = 5
@@ -257,8 +357,12 @@ gp_data = gp_data_from_model_and_path(gp_model, likelihood, x_plot, num_samples=
 plot_gp_data([gp_data], marker_size_lim=[1, 15])
 # -
 
+# We can also plot the derivative of the GP. Note that the projected Jacobian is not smooth since our path is not smooth either (jump projection direction = jump in Jacobian); however, the actual Jacobian should be smooth here (squared exponential kernel).
+
 gp_derivative_data = gp_derivative_data_from_model_and_path(gp_model, likelihood, x_plot, num_samples=0)
 plot_gp_data([gp_derivative_data], marker_size_lim=[5, 20], plot_train_data=False)
+
+# Compare with plotting along a slice of the dimension. Since we generated training data along the path of the robustified controller, the GP looks pretty untrained along a slice of the coordinates.
 
 # +
 # plot along axis
@@ -280,6 +384,9 @@ fig, ax = plot_gp_data([gp_grid_data], marker_size_lim=[5, 50])
 
 y_lim_0 = ax[0].get_ylim()
 y_lim_1 = ax[1].get_ylim()
+# -
+
+# Jacobian... not much going on away from the data points (this is good!)
 
 # +
 gp_derivative_grid_data = gp_derivative_data_from_model_and_path(gp_model, likelihood, x_grid, num_samples=0)
@@ -288,6 +395,11 @@ fig, ax = plot_gp_data([gp_derivative_grid_data], marker_size_lim=[5, 50], plot_
 ax[0].set_ylim(*y_lim_0)
 ax[1].set_ylim(*y_lim_1)
 plt.draw()
+# -
+
+# # Zero-Order GP-MPC
+#
+# We can add the GP model to the solver by simply adding it as an argument to the `ZoroAcados` function. Therefore we copy (important!) the robustified controller and then instantiate another solver object.
 
 # +
 from copy import deepcopy
@@ -306,6 +418,11 @@ zoro_solver.ocp_solver.set(N, "x",X_init[N,:])
 
 zoro_solver.solve()
 X,U,P = zoro_solver.get_solution()
+# -
+
+# ## Trained GP vs. no GP
+#
+# Plotting trained GP against no-GP, we see that the GP-MPC controller still has higher uncertainties than the robustified one. This is because the GP covariance, albeit being small where data points are, is *added* to the process noise, i.e., we make the controller even more cautious.
 
 # +
 fig, ax = base_plot(lb_theta=lb_theta)
@@ -317,6 +434,11 @@ plot_data_gp = EllipsoidTubeData2D(
 )
 add_plot_trajectory(ax, plot_data_nogp, color_fun=plt.cm.Oranges)
 add_plot_trajectory(ax, plot_data_gp, color_fun=plt.cm.Reds)
+# -
+
+# We can get a more insightful comparison by comparing the *conditioned* GP (i.e., with optimized hyper-parameters and data), to the *unconditioned* GP (i.e., with the same, optimized hyper-parameters and no data).
+#
+# As you can see, the hyper-parameter optimization also provides us with an estimate of the measurement noise for the data points, which corersponds to added process noise in our model (epistemic uncertainty).
 
 # +
 ocp_zoro_gpprior = deepcopy(ocp_zoro_nogp)
@@ -331,7 +453,11 @@ Sigma_GP_prior
 
 Sigma_W
 
+# Especially in the velocity component, this really makes a difference!
+
 Sigma_W+Sigma_GP_prior
+
+# Now we solve again with *unconditioned* GP and compare...
 
 # +
 # zoro_solver_gpprior = ZoroAcados(ocp_zoro_gpprior, sim, prob_x, Sigma_x0, Sigma_W+Sigma_GP_prior)
@@ -349,6 +475,11 @@ zoro_solver_gpprior.ocp_solver.set(N, "x",X_init[N,:])
 zoro_solver_gpprior.solve()
 
 X_gpprior,U_gpprior,P_gpprior = zoro_solver_gpprior.get_solution()
+# -
+
+# ## Zero-Order GP-MPC with unconditioned GP vs. conditioned GP
+#
+# Now we see that the training indeed paid off, and reduced uncertainty where we obtained data, i.e., we can go closer to the boundary while still being cautious.
 
 # +
 
